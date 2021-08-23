a167d05d54ab4b1103cfe5802ce0ee22e820a13a
niharikakanakala <niharikakanakala@gmail.com>
2021-08-24 02:22:33 +0530

    sign-up page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import SignInAndSignUpPage from './pages/login/sign-in-and-sign-up.component';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          niharika
-        </a>
-      </header>
+    <div>
+      <Switch>
+         <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
