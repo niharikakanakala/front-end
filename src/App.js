9854d7e9b4f2c8d17d25429de8c04b2e2fcd2dd3
niharikakanakala <niharikakanakala@gmail.com>
2021-08-25 12:36:47 +0530

    register

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, {useState} from 'react';
 import './App.css';
-
-import SignInAndSignUpPage from './pages/login/sign-in-and-sign-up.component';
-
-
+import Header from './components/Header/Header';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import Home from './components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import AlertComponent from './components/AlertComponent/AlertComponent';  
 function App() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
   return (
-    <div>
-      <Switch>
-         <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
+    <Router>
+    <div className="App">
+      <Header title={title}/>
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/login">
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/home">
+              <Home/>
+            </Route>
+          </Switch>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+        </div>
     </div>
+    </Router>
   );
 }
 
